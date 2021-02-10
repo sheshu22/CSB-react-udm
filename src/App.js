8094a299915c7533ce7ed6960d236a9db366a14e
Sheshasena Reddy <53706557+sheshu22@users.noreply.github.com>
2021-02-10 15:53:50 +0530

    challenge 1

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="div">
+        <h1>My Fav Foods</h1>
+        <ul>
+          <li>biryani</li>
+          <li>biryani</li>
+          <li>biryani</li>
+        </ul>
+      </div>
     </div>
   );
 }
