bc46366e72162f7e661a67de79ee9370f9e260cd
Sheshasena Reddy <53706557+sheshu22@users.noreply.github.com>
2021-02-11 02:05:25 +0530

    added styles and images

--- src/App.js
+++ src/App.js
@@ -10,15 +10,25 @@ export default function App() {
   let date = d.getFullYear();
   return (
     <div className="App">
+      <h1 className="div" contentEditable="true" spellCheck="false">
+        My Fav Foods
+      </h1>
       <div>
-        <h1 className="div" contentEditable="true" spellCheck="false">
-          My Fav Foods
-        </h1>
-        <ul>
-          <li>biryani</li>
-          <li>biryani</li>
-          <li>biryani</li>
-        </ul>
+        <img
+          src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/20190503-delish-pineapple-baked-salmon-horizontal-ehg-450-1557771120.jpg?crop=0.670xw:1.00xh;0.173xw,0&resize=480:*"
+          alt="img"
+          className="imgs"
+        ></img>
+        <img
+          src="https://www.diabetes.co.uk/wp-content/uploads/2019/01/iStock-10131071761-1.jpg"
+          alt="img2"
+          className="imgs"
+        ></img>
+        <img
+          src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/processed-food700-350-e6d0f0f.jpg?quality=90&resize=960,872"
+          alt="img3"
+          className="imgs"
+        ></img>
       </div>
       <h1>
         Hello {name} {lname}
