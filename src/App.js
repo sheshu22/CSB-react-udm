3621ef9d349f7ee8280a919c8f4d2a2ec71bb63e
Sheshasena Reddy <53706557+sheshu22@users.noreply.github.com>
2021-02-12 01:07:50 +0530

    import and export

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import Heading from "./Heading";
 import Images from "./Images";
 import Wishes from "./Wishes";
+import pi, { Math } from "./math.jsx";
 
 export default function App() {
   return (
@@ -10,6 +11,8 @@ export default function App() {
       <Heading />
       <Images />
       <Wishes />
+      {pi}
+      <Math />
     </div>
   );
 }
---
+++ src/math.jsx
@@ -0,0 +1,8 @@
+const pi = 3.14;
+
+function Math() {
+  return pi * 2;
+}
+
+export default pi;
+export { Math };
