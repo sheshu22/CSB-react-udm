81c0f6898996796effa97bd7b87934f5e38b37a3
Sheshasena Reddy <53706557+sheshu22@users.noreply.github.com>
2021-02-14 02:52:59 +0530

    math jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 import Heading from "./Heading";
 import Images from "./Images";
 import Wishes from "./Wishes";
-import pi, { Math } from "./math.jsx";
+import pi, { Math, Add, Sub, Mul } from "./math";
 
 export default function App() {
   return (
@@ -11,7 +11,6 @@ export default function App() {
       <Heading />
       <Images />
       <Wishes />
-      {pi}
       <Math />
     </div>
   );
--- src/math.jsx
+++ src/math.jsx
@@ -1,8 +1,23 @@
-const pi = 3.14;
+import React from "react";
 
-function Math() {
-  return pi * 2;
+function Add(a, b) {
+  return a + b;
+}
+function Sub(a, b) {
+  return a - b;
+}
+function Mul(a, b) {
+  return a * b;
 }
 
-export default pi;
-export { Math };
+export default function Math() {
+  return (
+    <ul>
+      <li>{Add(1, 2)}</li>
+      <li>{Sub(1, 2)}</li>
+      <li>{Mul(1, 2)}</li>
+    </ul>
+  );
+}
+
+export { Math, Add, Sub, Mul };
